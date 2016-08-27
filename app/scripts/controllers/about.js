56b0274cf84dbb5084f1913b57862776280e642f
oleksii-s <oleksii.s@myplanet.com>
2016-08-27 16:30:43 +0300

    Yo, initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loriflameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the loriflameApp
+ */
+angular.module('loriflameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loriflameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loriflameApp
+ */
+angular.module('loriflameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
